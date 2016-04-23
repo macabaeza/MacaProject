f8da5de98b5fb2b3b114c2d30f42eaabe239d8d0
macabaeza <macarenabaezav@yahoo.com>
2016-04-22 20:28:42 -0700

    added list

--- MacaProject/Factory.cs
+++ MacaProject/Factory.cs
@@ -28,5 +28,7 @@ namespace MacaProject
             return item;
 
         }
+
     }
+
 }
--- MacaProject/Program.cs
+++ MacaProject/Program.cs
@@ -11,36 +11,54 @@ namespace MacaProject
         static void Main(string[] args)
         {
             Console.WriteLine(" ***** Welcome to Amazon****");
-            Console.WriteLine("1. Create an Id");
-            Console.WriteLine("2. Name ");
-            Console.WriteLine("3. Description");
-            Console.WriteLine("0. Exit");
-
-            var option = Console.ReadLine();
-            switch (option)
+            string option;
+            do
             {
-                case "1":
-                    Console.Write("What is the Id Number?");
-                    var ItemId = Console.ReadLine();
+                Console.WriteLine("1. Create an Id");
+                Console.WriteLine("2. Name ");
+                Console.WriteLine("3. Description");
+                Console.WriteLine("4. Print Id ");
+                Console.WriteLine("0. Exit");
 
-                    var Item1 = Factory.CreateItem(123, " El Principito", " In 2000 Harcourt proudly reissued Antoine de Saint-Exupéry's masterpiece, The Little Prince, in a sparkling new format.");
+                option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        Console.Write("What is the Id Number?");
+                        var ItemId = Console.ReadLine();
 
-                    Console.WriteLine("Item Id: {0}, name:{1}, description: {2}",
-                        Item1.ItemId, Item1.ItemName, Item1.ItemDescription);
+                        var Item1 = Factory.CreateItem(123, " El Principito", " In 2000 Harcourt proudly reissued Antoine de Saint-Exupéry's masterpiece, The Little Prince, in a sparkling new format.");
 
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                case "0":
-                    Console.WriteLine("Good bye!");
-                    return;
-  
-                default:
-                    break;
+                        Console.WriteLine("Item Id: {0}, name:{1}, description: {2}",
+                            Item1.ItemId, Item1.ItemName, Item1.ItemDescription);
+
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        PrintItems();
+                        break;
+                    case "0":
+                        Console.WriteLine("Good bye!");
+                        return;
+
+                    default:
+                        break;
+                }
+            } while ( option!= "0" );
+        }
+
+        static void PrintItems()
+
+        {
+            foreach (var item in Factory.items)
+
+            {
+                Console.WriteLine("Id: {0}, Name: {1}",
+                    item.ItemId, item.ItemName);
+
             }
- 
         }
     }
-}
